7e44e9515d2f9fde34ba24c6cec0ef19c55195c4
daram ajay <ajayforchrist777@gmail.com>
2020-07-11 18:50:48 +0000

    project-finished

--- src/App.js
+++ src/App.js
@@ -8,22 +8,45 @@ const App = () => (<PersonList />)     //component which is an App to render the
 
 
 const PersonList = () => {          //actual personal list
+  const people = [
+    {
+      img:22,
+      name:"Mark",
+      job:"developer"
+    },
+    {
+      img:33,
+      name:"Peter",
+      job:"Actor"
+    },
+    {
+      img:52,
+      name:"John",
+      job:"Painter"
+    }
+  ];
   return (
-    <Person></Person>
+  <section>
+    <Person person={people[0]}/>
+    <Person person={people[1]}>Lorem Ipsum dolor sit amet.Suspendisse faucibus augue sapien tincidunt eros lobortis non. Phasellus porta pulvinar mauris id maximus.</Person>
+    <Person person={people[2]}/>
+  </section>
   )
-
 }
 
-const Person = () => {               //person
+const Person = (props) => {   
+  const{img,name,job} = props.person;
+  const {children} = props;           
   const url =
-    "https://randomuser.me/api/portraits/thumb/men/22.jpg";
+    `https://randomuser.me/api/portraits/thumb/men/${img}.jpg`;
 
     return(
       <div className="person">
         <img src={url}alt="" />
         <div>
-          <h4>name</h4>
-          <h4>Job</h4>
+    <h4>{name}</h4>
+          <h4>{job}</h4>
+          {children}
           </div>
         </div>
     )
